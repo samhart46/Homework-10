8ff91fffc76bb2512ee14eee402b5a0b849f1f9d
Samuel Hartman <s.hartman@setonhill.edu>
2020-05-04 00:41:57 -0400

    Initial Commit

---
+++ Homework 10/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework 10
+//
+//  Created by Sam Hartman on 5/4/20.
+//  Copyright © 2020 Sam Hartman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
